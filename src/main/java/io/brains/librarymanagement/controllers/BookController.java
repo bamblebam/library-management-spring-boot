fd006085a96ae6cebd667ea20259151289649384
bamblebam <burhanizrangwala@gmail.com>
2022-05-04 00:16:31 +0530

    fixed empty objects by adding getters and setters

--- src/main/java/io/brains/librarymanagement/controllers/BookController.java
+++ src/main/java/io/brains/librarymanagement/controllers/BookController.java
@@ -3,7 +3,6 @@ package io.brains.librarymanagement.controllers;
 import java.util.List;
 import java.util.Map;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/io/brains/librarymanagement/models/BookModel.java
+++ src/main/java/io/brains/librarymanagement/models/BookModel.java
@@ -28,6 +28,54 @@ public class BookModel {
     public BookModel() {
     }
 
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return this.genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPublisher() {
+        return this.publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public BookModel(String title, String author, String genre, String publisher, String description) {
         this.title = title;
         this.author = author;
