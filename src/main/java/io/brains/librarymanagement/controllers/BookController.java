278f540d492cab7db822950a8c4d78648c96a012
bamblebam <burhanizrangwala@gmail.com>
2022-05-04 00:37:46 +0530

    update and delete functionality done

--- src/main/java/io/brains/librarymanagement/controllers/BookController.java
+++ src/main/java/io/brains/librarymanagement/controllers/BookController.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -38,9 +40,40 @@ public class BookController {
 
     //Create a book
     @PostMapping("/books/create")
-    public void createBook(@RequestBody Map<String,Object> payload) throws Exception {
+    public BookModel createBook(@RequestBody Map<String,Object> payload) throws Exception {
         BookModel bookModel = new BookModel(payload.get("title").toString(), payload.get("author").toString(), payload.get("genre").toString(), payload.get("publisher").toString(), payload.get("description").toString());
         bookModelRepository.save(bookModel);
-        System.out.println("Saved");
+        return bookModel;
+    }
+
+    //Update a book
+    @PatchMapping("/books/update/{id}")
+    public BookModel updateBook(@PathVariable("id") String id, @RequestBody Map<String,Object> payload) throws Exception {
+        BookModel bookModel = bookModelRepository.findById(id).orElseThrow(() -> new Exception("Book not found"));
+        if(payload.containsKey("title")) {
+            bookModel.setTitle(payload.get("title").toString());
+        }
+        if(payload.containsKey("author")) {
+            bookModel.setAuthor(payload.get("author").toString());
+        }
+        if(payload.containsKey("genre")) {
+            bookModel.setGenre(payload.get("genre").toString());
+        }
+        if(payload.containsKey("publisher")) {
+            bookModel.setPublisher(payload.get("publisher").toString());
+        }
+        if(payload.containsKey("description")) {
+            bookModel.setDescription(payload.get("description").toString());
+        }
+        bookModelRepository.save(bookModel);
+        return bookModel;
+    }
+
+    //Delete a book
+    @DeleteMapping("/books/delete/{id}")
+    public String deleteBook(@PathVariable("id") String id) throws Exception {
+        BookModel bookModel = bookModelRepository.findById(id).orElseThrow(() -> new Exception("Book not found"));
+        bookModelRepository.delete(bookModel);
+        return "Book deleted successfully";
     }
 }
