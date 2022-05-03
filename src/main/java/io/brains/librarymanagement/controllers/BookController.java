dfca6b3c0f00471842b759c9cdba0eddab78c3f7
bamblebam <burhanizrangwala@gmail.com>
2022-05-04 00:06:36 +0530

    bug returning empty objects

--- src/main/java/io/brains/librarymanagement/controllers/BookController.java
+++ src/main/java/io/brains/librarymanagement/controllers/BookController.java
@@ -3,8 +3,10 @@ package io.brains.librarymanagement.controllers;
 import java.util.List;
 import java.util.Map;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +26,15 @@ public class BookController {
     //Get all the books
     @GetMapping("/books")
     public List<BookModel> getBooks() throws Exception {
+        // System.out.println(bookModelRepository.findAll());
         return bookModelRepository.findAll();
+    }
+
+    //Get a single book by id
+    @GetMapping("/books/{id}")
+    public BookModel getBook(@PathVariable("id") String id) throws Exception {
+        // System.out.println(bookModelRepository.findById(id));
+        return bookModelRepository.findById(id).orElseThrow(() -> new Exception("Book not found"));
     }
 
     //Create a book
